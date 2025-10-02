4f6eb724fc7fe2a62aaa806293667453fa56240c
supercalifragilisticexpialidociouseven <3135498@smsd.org>
2025-10-02 08:29:22 -0500

    Update ClimberSubsystem.java
    climber command

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.climber;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase{
+    private final SparkMax climberMotor;
+    ClimberSubsystem() {
+        climberMotor = new SparkMax(17, MotorType.kBrushless);
+    }
+    
+    public Command moveClimberOut() {
+        return runEnd(
+            () -> {
+                climberMotor.set(0.1);
+            },
+            () -> {
+                climberMotor.set(0.0);
+            }
+        );
+    }
+}
+   
