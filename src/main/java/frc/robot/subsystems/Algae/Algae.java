4e86206c9645b7935a2a3b8d476d6dfe45f16836
ArnoldoAlvbh <3127118@smsd.org>
2025-10-02 15:23:01 -0500

    Create Algae.java

---
+++ src/main/java/frc/robot/subsystems/Algae/Algae.java
@@ -0,0 +1,37 @@
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.vision.PhotonVisionSubsystem;
+import frc.robot.subsystems.vision.QuestNavSubsystem;
+import org.littletonrobotics.junction.Logger;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+
+public class Algae extends SubsystemBase{ 
+
+private SparkMax leftMotor;
+private SparkMax rightMotor; 
+private RelativeEncoder leftMotorEncoder;
+private RelativeEncoder rightMootorEncoder;  
+
+
+
+
+
+}
