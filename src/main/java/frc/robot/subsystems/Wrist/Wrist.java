2d190a0ce7a549cf56c1c0641b4e879aab0ac737
Alex-1R <3125182@smsd.org>
2025-10-06 17:45:08 -0500

    Update Wrist.java

--- src/main/java/frc/robot/subsystems/Wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist/Wrist.java
@@ -39,11 +39,24 @@ public class Wrist extends SubsystemBase{
     }    
 
     public static class WristMotorConstants{
+      public static final double REEF_ACTIVE_TIME = 0.5;
+      public static final double STATION_ACTIVE_TIME = 8.0;
+
+      public static final double CORAL_INTAKE_SPEED = 0.6;
+      public static final double CORAL_EJECT_SPEED = -0.4;
+      public static final double ALGAE_INTAKE_MAX_SPEED = 0.5;
+      
       public static final Rotation2d LEVEL_1_CORAL_WRIST_POSITION = Rotation2d.fromDegrees(0);
       public static final Rotation2d LEVEL_MID_CORAL_WRIST_POSITION = Rotation2d.fromDegrees(-15);
       public static final Rotation2d LEVEL_4_CORAL_WRIST_POSITION = Rotation2d.fromDegrees(0);
       public static final Rotation2d INTAKE_CORAL_WRIST_POSITION = Rotation2d.fromDegrees(35);
       
+      public static final Rotation2d CORAL_WRIST_STARTING_POSITION = Rotation2d.fromDegrees(72);
+      public static final Rotation2d CORAL_WRIST_STOW_POSITION = Rotation2d.fromDegrees(60);
+  
+      public static final double CORAL_ENCODER_ROTATIONS_TO_DEGREES_MULTIPLIER = 72/-5.2857;
+      public static final double CORAL_POSITION_OFFSET = 72;
+
       // Preset positions for Arm with Coral
       public static final double CORAL_PRESET_L1 = 0;
       public static final double CORAL_PRESET_L2 = 0.13;
@@ -64,6 +77,25 @@ public class Wrist extends SubsystemBase{
       // Constant for gear ratio (the power that one motor gives to gear)
        private static final double ARM_RATIO = (12.0 / 60.0) * (20.0 / 60.0) * (18.0 / 48.0);
 
+
+       public static final int ALGAE_PDP_CHANNEL = 11;
+       public static final int CORAL_PDP_CHANNEL = 13;
+       public static final int CORAL_WRIST_PDP_CHANNEL = 12;
+
+       public static final int ALGAE_IN_CURRENT_THRESHOLD = 25;
+       public static final int CORAL_IN_CURRENT_THRESHOLD = 10;
+       public static final int CORAL_EJECT_CURENT_THRESHOLD = 2;
+
+       public static final double CORAL_WRIST_STATIC_GAIN = 0.01;
+       public static final double CORAL_WRIST_GRAVITY_GAIN = 0.6;
+       public static final double CORAL_WRIST_VELOCITY_GAIN = 0.0;
+    
+       public static final double CORAL_WRIST_PROPORTIONAL_GAIN = 8;
+       public static final double CORAL_WRIST_INTEGRAL_GAIN = 0.1;
+       public static final double CORAL_WRIST_DERIVATIVE_GAIN = 0.2;
+
+       public static final double CORAL_WRIST_MAX_VELOCITY_RADIANS_PER_SECOND = Math.PI / 4;
+       public static final double CORAL_WRIST_MAX_ACCELERATION_RADIANS_PER_SECOND_SQUARED = Math.PI;
     }
     public Command runMotors() {
         return runOnce(
