8162005d233d2b849497c06cbc755d26b085846e
Zach4346074545723 <3091322@smsd.org>
2025-10-09 08:37:05 -0500

    added constants for the climber subsystem

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -6,6 +6,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase{
+    public static class ClimberConstants{
+        public static final double EXTEND_SPEED = 0.1;
+        public static final double RETRACT_SPEED = -0.1;
+    }
     private final SparkMax climberMotor;
     public ClimberSubsystem() {
         climberMotor = new SparkMax(17, MotorType.kBrushless);
@@ -16,7 +20,7 @@ public class ClimberSubsystem extends SubsystemBase{
     public Command moveClimberOut() {
         return runEnd(
             () -> {
-                climberMotor.set(0.1);
+                climberMotor.set(ClimberConstants.EXTEND_SPEED);
             },
             () -> {
                 climberMotor.set(0.0);
@@ -26,11 +30,11 @@ public class ClimberSubsystem extends SubsystemBase{
     /**
      * moves the cliber arm in, twords the robot so it can hold on to the cage
      * or so it can be out of the way
-    */
+     */
     public Command moveClimberIn() {
         return runEnd(
             () -> {
-                climberMotor.set(-0.1);
+                climberMotor.set(ClimberConstants.RETRACT_SPEED);
             },
             () -> {
                 climberMotor.set(0.0);
