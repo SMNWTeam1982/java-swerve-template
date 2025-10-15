31f54dd7331bb3f14eb8f0ef2f32902fa70197b8
supercalifragilisticexpialidociouseven <3135498@smsd.org>
2025-10-15 08:34:53 -0500

    small changes to AlgaeSubsystem

--- src/main/java/frc/robot/subsystems/Algae/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Algae/AlgaeSubsystem.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.Algae;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.subsystems.Elevator.ElevatorSubsystem.ElevatorConstants;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
@@ -13,53 +13,58 @@ public class AlgaeSubsystem extends SubsystemBase{
 
 private final SparkMax leftMotor;
 private final SparkMax rightMotor; 
-private final RelativeEncoder leftMotorEncoder;
-private final RelativeEncoder rightMotorEncoder;  
 
+public static class AlgaeMotorConstants{    
+  public static final double ALGAE_INTAKE_MAX_SPEED = 0.5;
+  public static final double ALGAE_PDP_CHANNEL = 11;
+  public static final double ALGAE_IN_CURRENT_THRESHOLD = 25;
 
-
-
-
-
-
-
-public static final SparkBaseConfig LEFT_MOTOR_CONFIG = 
+  public static final SparkBaseConfig LEFT_MOTOR_CONFIG = 
   new SparkMaxConfig().smartCurrentLimit(35).idleMode(SparkBaseConfig.IdleMode.kCoast);
   
 public static final SparkBaseConfig RIGHT_MOTOR_CONGFIG =  
   new SparkMaxConfig().smartCurrentLimit(35).idleMode(SparkBaseConfig.IdleMode.kCoast);
  
 
+}
+private final SparkMax rightleadMotor = new SparkMax(14, SparkMax.MotorType.kBrushless);// initilizes lead moter motor 
+    private final RelativeEncoder rightleadEncoder = rightleadMotor.getEncoder();
+
+    private final SparkMax leftfollowingMotor = new SparkMax(12, SparkMax.MotorType.kBrushless);
+    private final RelativeEncoder leftfollowingEncoder = leftfollowingMotor.getEncoder();
 
 
 
- public AlgaeSubsystem() {
-    leftMotor = new SparkMax(13, SparkMax.MotorType.kBrushless); // initilizes pivot otor 
-    leftMotor.configure(LEFT_MOTOR_CONFIG, 
+
+
+
+  public AlgaeSubsystem() {
+    rightleadMotor.configure(
+        AlgaeMotorConstants.LEAD_MOTOR_CONFIG,
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters
+    );
+  
+    
+   
+    leftfollowingMotor.configure(
+      AlgaeSubsystem.LEFT_FOLLOWING_MOTOR_CONFIG.follow(14,true),
         SparkBase.ResetMode.kResetSafeParameters, 
         SparkBase.PersistMode.kPersistParameters);
         
-    rightMotor = new SparkMax(14, SparkMax.MotorType.kBrushless); // initilizes intake motor 
+    rightMotor = new SparkMax(14, SparkMax.MotorType.kBrushless); 
+       AlgaeMotorConstants.LEAD_MOTOR_CONFIG.follow(14,true);
        
-        rightMotor.configure(RIGHT_MOTOR_CONGFIG, 
+       rightMotor.configure(
+        AlgaeSubsystem.RIGHT_LEAD_MOTOR_CONGFIG.lead(14,true),
         SparkBase.ResetMode.kResetSafeParameters, 
         SparkBase.PersistMode.kPersistParameters);
 
-        leftMotorEncoder = leftMotor.getEncoder();
-        leftMotorEncoder.setPosition(0);
-
-        rightMotorEncoder = rightMotor.getEncoder();
-        rightMotorEncoder.setPosition(0);
+        
         
  };  
 
- public static class AlgaeMotorConstants{    
 
-
-    public static final double ALGAE_INTAKE_MAX_SPEED = 0.5;
-    public static final double ALGAE_PDP_CHANNEL = 11;
-    public static final double ALGAE_IN_CURRENT_THRESHOLD = 25;
- }
 
  public Command runintakeMotors() {
     return runOnce(
